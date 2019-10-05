75ff7546b6e1d9ec02b7e2c7e80fe5340d09c9bb
kaku <scugjs@gmail.com>
2019-10-05 09:40:38 +0800

    [add] reference to array

--- cpp_grammar_related/reference.cpp
+++ cpp_grammar_related/reference.cpp
@@ -1,26 +1,64 @@
 //
-//  filename.c
+//  filename.cpp
 //  clangSkill
 //
 //  Created by kaku on 201x/xx/xx.
 //  Copyright © 2019 kaku. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
 
-#define PRINT_INT(i) printf(#i ": %d\n", i)
-#define PRINT_CHR(c) printf(#c ": %c\n", c)
-#define PRINT_STR(s) printf(#s ": %s\n", s)
+using namespace std;
 
-void referenceFunc(char &ch);
+void swap1(int *, int *);
+void swap2(int &, int &);
+void show(int, int); 
+void show_array(int (&arr)[5]);
 
 int main() {
-    char ch = 'a';
-    PRINT_CHR(ch);
-    referenceFunc(ch);
-    PRINT_CHR(ch);
+    int i = 10;
+    int *p = &i;
+    int & r = *p;
+    cout << r << endl;
+
+    int j = 11;
+    p = &j;  // reference still represent i!
+    cout << "Change ponter points to j!\n";
+    cout << r << endl;
+    show(i, j);
+
+    swap1(&i, &j);
+    show(i, j);
+    swap2(i, j);
+    show(i, j);
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    show_array(arr);
 }
 
-void referenceFunc(char &ch) {
-    ch = 'A';
+void swap1(int * p, int * q) {
+    int temp;
+
+    temp = *p;
+    *p = *q;
+    *q = temp;
+}
+
+void swap2(int & a, int & b) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+
+void show(int i, int j) {
+    cout << "i: "
+         << i
+         << "\t"
+         << "j: "
+         << j
+         << endl;
+}
+
+void show_array(int (&arr)[5]) {
+    cout << "Reference to Array!\n";
 }
